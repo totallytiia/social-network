b4434c43f49a95db1d6b926cd3c3513d46ba6576
totallytiia <tlaukkan@student.hive.fi>
2023-07-26 14:47:51 +0300

    some comment stuff

--- social-network/src/Components/Posts/CommentsSection.tsx
+++ social-network/src/Components/Posts/CommentsSection.tsx
@@ -16,11 +16,16 @@ interface IComments {
 }
 
 
-export default function CommentsSection() {
+export default function CommentsSection(props: any) {
 
 	// fetch comments data from backend
 	const [commentsData, setCommentsData] = useState({
 		comments: [] as IComments[],
+	});
+
+	const [comment, setComment] = useState({
+		post_id: 0,
+		comment: "",
 	});
 
 	useEffect(() => {
@@ -40,14 +45,68 @@ export default function CommentsSection() {
 		fetchCommentsData();
 	}, []);
 
+	const handleCommentSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log(comment);
+		const FD = new FormData();
+
+		FD.append("post_id", comment.post_id.toString() as string);
+		FD.append("comment", comment.comment as string);
+
+		const response = await fetch('http://localhost:8080/api/comments/create', {
+			method: 'POST',
+			credentials: 'include',
+			body: FD,
+		});
+
+		(document.getElementById('commentBox') as HTMLInputElement).value = "";
+
+
+		if (response.status === 201) {
+			console.log('Comment created!');
+			// Send await response.json() to the parent
+			const comment = await response.json();
+
+		} else {
+			console.log('Error creating comment!');
+			console.log(response);
+		}
+	};
+
 	return (
 		<>
-			<div>
+			<div className="ml-8 mr-6 my-2">
+				<form
+					onSubmit={(e) => handleCommentSubmit(e)}
+					className="flex flex-cols"
+					action="">
+					<input className="bg-gray-50 px-4 text-sm outline-none rounded-full w-full"
+						type="text"
+						id="commentBox"
+						placeholder="Write a comment"
+						onChange={(e) => {
+							setComment({
+								post_id: props.post_id,
+								comment: e.target.value,
+							});
+						}} />
+					<label className="my-auto cursor-pointer" htmlFor="commentSubmit">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="gray" className="w-5 h-5 ml-1 my-auto">
+							<path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+						</svg>
+					</label>
+					<input
+						id="commentSubmit"
+						className="hidden"
+						type="submit"
+					/>
+
+				</form>
 				{/* get all comments  */}
-				{commentsData.comments.map((comment: any) => (
+				{/* {commentsData.comments.map((comment: any) => (
 					<Comment comment={comment} />
-				))}
-			</div>
+				))} */}
+			</div >
 		</>
 	)
 }
