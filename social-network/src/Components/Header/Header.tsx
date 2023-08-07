add03df1fa5a575f64f61bbe9f22e9f3e70ab42d
totallytiia <tlaukkan@student.hive.fi>
2023-08-07 15:54:29 +0300

    notifications stuff

--- social-network/src/Components/Header/Header.tsx
+++ social-network/src/Components/Header/Header.tsx
@@ -30,6 +30,20 @@ export default function Header() {
         getNotifications();
     }, []);
 
+    const openNotifications = () => {
+        const notificationsContainer = document.querySelector(
+            '.NOTIFICATIONS-CONTAINER'
+        ) as HTMLDivElement;
+        notificationsContainer.classList.toggle('hidden');
+    };
+
+    // hide the parent div of the notification
+    const hideNotification = (id: number) => {
+        console.log("this" + id);
+    };
+
+
+
     async function Logout() {
         const res = await fetch('http://localhost:8080/api/users/logout', {
             method: 'GET',
@@ -41,11 +55,16 @@ export default function Header() {
         }
     }
 
+
+
+
+
     return (
         <header className="HEADER bg-white sticky top-0 shadow-lg z-50">
             <nav className="NAVIGATION grid grid-cols-2 gap-3 items-center px-2 py-2 mx-2">
                 <div className="LOGO">
-                    <h1 className="LOGO-TEXT text-black text-2xl font-bold">
+                    <h1 onClick={() => (window.location.href = '/')}
+                        className="LOGO-TEXT cursor-pointer text-black text-2xl font-bold">
                         SOCIAL NETWORK
                     </h1>
                 </div>
@@ -107,18 +126,9 @@ export default function Header() {
                         </div>
                     </div>
                 </section>
-                <div className="space-x-2 text-xs justify-end DESKTOP-MENU hidden lg:flex">
+                <div className="DESKTOP-MENU space-x-2 text-xs justify-end  hidden lg:flex">
                     <div>
-                        <Link
-                            to="/"
-                            className="text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full"
-                        >
-                            +
-                        </Link>
-                    </div>
-                    <div>
-                        <Link
-                            to="/chat"
+                        <div
                             className="text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full"
                         >
                             <svg
@@ -135,40 +145,128 @@ export default function Header() {
                                     d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
                                 />
                             </svg>
-                        </Link>
+                        </div>
                     </div>
                     <div>
-                        <Link
-                            to="/notifications"
-                            className="text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full"
-                        >
+                        <div onClick={openNotifications}
+                            className="cursor-pointer relative text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full">
                             Notifications
-                        </Link>
+                            {notifications.length > 0 && (
+                                <div className="flex absolute right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+                                    {notifications.length}
+                                </div>
+                            )}
+                        </div>
+                        <div className="NOTIFICATIONS-CONTAINER hidden flex absolute top-14 right-2 bg-white shadow-lg rounded-lg w-96">
+                            <div className="NOTIFICATIONS-CONTAINER-HEADER flex flex-col  w-full px-2 py-1 border-b border-gray-200">
+                                <h1 className="text-lg font-bold">
+                                    Notifications
+                                </h1>
+                                {notifications.map((notification) => (
+                                    <div
+                                        key={notification.id}
+                                        className="NOTIFICATION flex flex-col"
+                                    >
+                                        <div id="test" className="NOTIFICATION-TEXT flex flex-row justify-between bg-blue-50 p-2 my-1 rounded-md" >
+                                            <div>
+                                                <p className="text-sm">
+                                                    text here
+                                                    {notification.message}
+                                                </p>
+                                                <p className="text-xs text-gray-500">
+                                                    14:00
+                                                    {notification.createdAt}
+                                                </p>
+                                            </div>
+                                            <div className="NOTIFICATION-ACTIONS flex-col flex">
+                                                <button className="text-xs font-bold"
+                                                    onClick={(e) => hideNotification(notification.id)}>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        strokeWidth={1.5}
+                                                        stroke="#e50000"
+                                                        className="w-4 h-4"
+                                                    >
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div >
+                                        <div id="test" className="NOTIFICATION-TEXT flex flex-row justify-between bg-blue-50 p-2 my-1 rounded-md" >
+                                            <div>
+                                                <p className="text-sm">
+                                                    text here
+                                                    {notification.message}
+                                                </p>
+                                                <p className="text-xs text-gray-500">
+                                                    15:00
+                                                    {notification.createdAt}
+                                                </p>
+                                            </div>
+                                            <div className="NOTIFICATION-ACTIONS flex-col flex">
+                                                <button className="text-xs font-bold"
+                                                    onClick={(e) => hideNotification(notification.id)}>
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        strokeWidth={1.5}
+                                                        stroke="#e50000"
+                                                        className="w-4 h-4"
+                                                    >
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                                                        />
+                                                    </svg>
+                                                </button>
+                                            </div>
+                                        </div >
+                                    </div>
+                                ))}
+
+                                {/* <button className="text-xs font-bold">
+                                        Mark all as read
+                                    </button> */}
+                            </div>
+                        </div>
+
+
                     </div>
-                    <div>
+                    <div
+                        className="text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full"
+                    >
                         <Link
                             to="/groups"
-                            className="text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full"
                         >
                             Groups
                         </Link>
                     </div>
-                    <div>
+                    <div
+                        className="text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full">
                         <Link
                             to="/profile"
-                            className="text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full"
                         >
                             Profile
                         </Link>
                     </div>
-                    <div onClick={Logout}>
+                    <div onClick={Logout}
+                        className="text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full"
+                    >
                         <Link
                             to="/"
-                            className="text-black bg-gray-200 hover:bg-gray-300 focus:bg-gray-300  py-2 px-3 rounded-full"
                         >
                             Log out
                         </Link>
                     </div>
+
                 </div>
             </nav>
             <style>{`
@@ -190,6 +288,6 @@ export default function Header() {
         align-items: center;
       }
     `}</style>
-        </header>
+        </header >
     );
 }
