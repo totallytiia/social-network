d2a36cfeb527a7a4f0c09931c8471342ba51f5e6
totallytiia <tlaukkan@student.hive.fi>
2023-08-08 19:30:19 +0300

    some group stuff

--- social-network/src/Components/Group/CreateGroup.tsx
+++ social-network/src/Components/Group/CreateGroup.tsx
@@ -1,27 +1,31 @@
-import React, { useEffect } from 'react';
+
 
 export default function CreateGroup() {
 	return (
-		<div className="create-group">
+		<div className="create-group flex flex-col align-center justify-center items-center">
 			<div className="create-group__header">
-				<h2>Create Group</h2>
 			</div>
-			<div className="create-group__form">
-				<form>
-					<div className="form__group">
-						<label htmlFor="name">Name</label>
-						<input type="text" name="name" id="name" />
+			<div className="create-group__form w-full p-2">
+				<form className="">
+					<label htmlFor="">Create group</label>
+					<div className="form__group mt-1">
+						<input className="w-full" type="text" name="name" id="name" placeholder="Group's name" />
 					</div>
-					<div className="form__group">
-						<label htmlFor="description">Description</label>
-						<textarea name="description" id="description" />
+					<div className="form__group mt-2">
+						<textarea className="rounded-lg text-sm p-2 w-full" name="description" id="description" placeholder="Describe the group" />
 					</div>
-					<div className="form__group">
-						<label htmlFor="private">Private</label>
-						<input type="checkbox" name="private" id="private" />
+					<div className="form__group flex flex-row items-center gap-1 mt-1">
+						<label className="mb-0" htmlFor="private">Private</label>
+						<input className="items-center" type="checkbox" name="private" id="private" />
+					</div>
+					<div className="form__group mt-2">
+						<input className="w-full" type="text" name="invite" id="invite" placeholder="Invite friends" />
+					</div>
+					<div className="form__group button-custom text-center bg-blue-100 font-bold rounded-xl mt-2 p-2">
+						<button>Create group</button>
 					</div>
 				</form>
-			</div>
-		</div>
+			</div >
+		</div >
 	);
 }
--- social-network/src/Components/Posts/GroupsSidebar.tsx
+++ social-network/src/Components/Posts/GroupsSidebar.tsx
@@ -1,16 +1,39 @@
 import Group from "../Group/Group"
+import CreateGroup from "../Group/CreateGroup"
+import { useState } from "react";
 
 export default function GroupsSidebar() {
+    const [createGroup, setCreateGroup] = useState(false);
+
+    function handleCreateGroup() {
+        setCreateGroup(current => !current);
+    }
+
     return (
         <>
-            <div className="order-1 lg:mx-0 mx-auto w-9/12 lg:max-w-xs">
+            <div className=" align-center w-full">
                 <div className="bg-blue-50 rounded-xl mt-6 p-4">
                     <h1 className="font-bold text-xl text-black mb-2">
                         GROUPS
                     </h1>
+                    {!createGroup && (
+                        <div className="CREATE-EVENT">
+                            <button onClick={() => handleCreateGroup()} className="flex flex-row text-lg font-bold items-center gap-1 bg-blue-50 py-2 pl-2 pr-4 rounded-full hover:bg-blue-100">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8">
+                                    <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z" clipRule="evenodd" />
+                                </svg>
+                                <p className="text-bold font-lg">
+                                    Create group
+                                </p>
+                            </button>
+                        </div>
+                    )}
+                    <div className=" mx-auto">
+                        {createGroup && <CreateGroup />}
+                    </div>
                     <Group />
                 </div>
-            </div>
+            </div >
         </>
     );
 }
