6ae3be0d0e3b992a72a7adb7818c6ce719c3e9ab
totallytiia <tlaukkan@student.hive.fi>
2023-08-15 14:23:54 +0300

    chat base started

--- social-network/src/Components/Page/Page.tsx
+++ social-network/src/Components/Page/Page.tsx
@@ -2,6 +2,7 @@ import Header from '../Header/Header';
 import Posts from '../Posts/Posts';
 import Footer from '../Footer/Footer';
 import FourOneFour from '../FourOneFour/FourOneFour';
+import Chat from '../Chat/Chat';
 import Groups from '../Group/GroupsPage';
 import Group from '../Group/Group';
 
@@ -15,10 +16,10 @@ function Page() {
             <Routes>
                 <Route path="/" element={<Posts />} />
                 <Route path="/user/:id" element={<User />} />
-                {/*                 
-                <Route path="/chat" element={<Chat />} />
-                <Route path="/notifications" element={<Notifications />} /> */}
-                <Route path="/groups" element={<Groups />} />
+                {
+                    <Route path="/chat" element={<Chat />} />
+              /*  <Route path="/notifications" element={<Notifications />} /> * /}
+                    < Route path="/groups" element={<Groups />} />
                 <Route path="/group/:id" element={<Group />} />
                 {/* <Route path="/new-post" element={<NewPost />} /> */}
                 <Route path="*" element={<FourOneFour />} />
