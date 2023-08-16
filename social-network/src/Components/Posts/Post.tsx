5dfedfdd238816f7ef311b1b1e7275ec2d78a65f
tiialaukkanen <tiia.laukkanen@gritlab.ax>
2023-08-16 16:52:46 +0300

    idkk

--- social-network/src/Components/Posts/Post.tsx
+++ social-network/src/Components/Posts/Post.tsx
@@ -4,6 +4,12 @@ import { UserContext } from '../App/App';
 import ErrorNotification from '../Notification/ErrorNotification';
 import { render } from '@testing-library/react';
 import ProfileIcon from '../Profile/ProfileIcon';
+import {
+    HandThumbUpIcon,
+    HandThumbDownIcon,
+    ChatBubbleOvalLeftIcon,
+} from '@heroicons/react/24/solid';
+
 interface PostProps {
     deletePost: (id: number) => void;
     postInput: {
@@ -128,7 +134,7 @@ export default function Post({ postInput, deletePost }: PostProps) {
 
     return (
         <div>
-            <div className="mt-4 mx-6 mb-0 p-5 bg-blue-50 rounded-xl">
+            <div className="mt-4 mx-6 mb-0 p-4 bg-blue-50 rounded-xl">
                 <div className="flex justify-between">
                     <div className="flex">
                         <div
@@ -200,52 +206,29 @@ export default function Post({ postInput, deletePost }: PostProps) {
                     <div className="flex gap-4">
                         <button
                             onClick={(e) => handleLike(e)}
-                            className="flex gap-2"
+                            className="flex gap-1"
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
+                            <HandThumbUpIcon
                                 className={`w-5 h-5 my-auto ${
                                     post.liked === 1 ? 'fill-green-500' : ''
                                 }`}
-                            >
-                                <path d="M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75 2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23h-.777zM2.331 10.977a11.969 11.969 0 00-.831 4.398 12 12 0 00.52 3.507c.26.85 1.084 1.368 1.973 1.368H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 01-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227z" />
-                            </svg>
+                            />
 
                             <p>{post.likes}</p>
                         </button>
                         <button
                             onClick={(e) => handleDislike(e)}
-                            className="flex gap-2"
+                            className="flex gap-1"
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
+                            <HandThumbDownIcon
                                 className={`w-5 h-5 my-auto ${
                                     post.liked === -1 ? 'fill-red-600' : ''
                                 }`}
-                            >
-                                <path d="M15.73 5.25h1.035A7.465 7.465 0 0118 9.375a7.465 7.465 0 01-1.235 4.125h-.148c-.806 0-1.534.446-2.031 1.08a9.04 9.04 0 01-2.861 2.4c-.723.384-1.35.956-1.653 1.715a4.498 4.498 0 00-.322 1.672V21a.75.75 0 01-.75.75 2.25 2.25 0 01-2.25-2.25c0-1.152.26-2.243.723-3.218C7.74 15.724 7.366 15 6.748 15H3.622c-1.026 0-1.945-.694-2.054-1.715A12.134 12.134 0 011.5 12c0-2.848.992-5.464 2.649-7.521.388-.482.987-.729 1.605-.729H9.77a4.5 4.5 0 011.423.23l3.114 1.04a4.5 4.5 0 001.423.23zM21.669 13.773c.536-1.362.831-2.845.831-4.398 0-1.22-.182-2.398-.52-3.507-.26-.85-1.084-1.368-1.973-1.368H19.1c-.445 0-.72.498-.523.898.591 1.2.924 2.55.924 3.977a8.959 8.959 0 01-1.302 4.666c-.245.403.028.959.5.959h1.053c.832 0 1.612-.453 1.918-1.227z" />
-                            </svg>
+                            />
                             <p>{post.dislikes}</p>
                         </button>
-                        <button className="flex gap-2">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-5 h-5 my-auto"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z"
-                                />
-                            </svg>
+                        <button className="flex gap-1">
+                            <ChatBubbleOvalLeftIcon className="w-5 h-5 my-auto" />
                             <p>
                                 {post.comments !== null
                                     ? post.comments.length
