9959aa77042b620962c0c90217e5653375fa78cf
tiialaukkanen <tiia.laukkanen@gritlab.ax>
2023-08-21 13:01:13 +0300

    remove some stuff

--- social-network/src/Components/Header/Header.tsx
+++ social-network/src/Components/Header/Header.tsx
@@ -103,61 +103,6 @@ export default function Header() {
                         SOCIAL NETWORK
                     </h1>
                 </div>
-                {/* <section className="MOBILE-MENU flex lg:hidden justify-end">
-                    <div
-                        className="HAMBURGER-ICON space-y-2 "
-                        onClick={() => setIsNavOpen((prev) => !prev)}
-                    >
-                        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                        <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-                    </div>
-
-                    <div className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}>
-                        <div
-                            className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
-                            onClick={() => setIsNavOpen(false)}
-                        >
-                            <svg
-                                className="h-8 w-8 text-gray-600"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            >
-                                <line x1="18" y1="6" x2="6" y2="18" />
-                                <line x1="6" y1="6" x2="18" y2="18" />
-                            </svg>
-                        </div>
-                        <div
-                            onClick={() => setIsNavOpen((prev) => !prev)}
-                            className="NAVIGATION-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]"
-                        >
-                            <Link
-                                to="/groups"
-                                className="text-2xl font-extrabold"
-                            >
-                                Groups
-                            </Link>
-                            <button
-                                onClick={() =>
-                                    (window.location.href = `/user/${userData.id}`)
-                                }
-                                className="text-2xl font-extrabold"
-                            >
-                                Profile
-                            </button>
-                            <button
-                                onClick={Logout}
-                                className="text-2xl font-extrabold"
-                            >
-                                Logout
-                            </button>
-                        </div>
-                    </div>
-                </section> */}
                 <div className="DESKTOP-MENU space-x-3 text-xs justify-end flex lg:flex">
                     <div>
                         <div
